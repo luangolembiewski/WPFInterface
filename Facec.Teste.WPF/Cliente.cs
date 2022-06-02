568caf6d89babbab2d46f5dbf6e28c838a7a5d1e
Luan Golembiewski <luangolembiewski74@gmail.com>
2022-06-01 22:37:09 -0300

    att

---
+++ Facec.Teste.WPF/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Facec.Teste.WPF
+{
+    public class Cliente
+    {
+
+        [JsonPropertyName("id")]
+        public int id { get; set; } = 0;
+
+        [JsonPropertyName("documento")]
+        public string Documento { get; set; } = string.Empty;
+
+        [JsonPropertyName("nome")]
+        public string Nome { get; set; } = string.Empty;
+
+        public Cliente() { }
+
+        public Cliente(int id, string documento, string nome)
+        {
+            id = id;
+            Documento = documento;
+            Nome = nome;
+        }
+    }
+}
